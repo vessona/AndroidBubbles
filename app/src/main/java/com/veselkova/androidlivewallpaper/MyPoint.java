a89fdb8e34d8ea0cb5dbcb03d1877e7bdfc7fc09
Sofi <vessona2000@gmail.com>
2018-06-28 01:39:54 -0700

    test commit #2

--- app/src/main/java/com/veselkova/androidlivewallpaper/MyPoint.java
+++ app/src/main/java/com/veselkova/androidlivewallpaper/MyPoint.java
@@ -1,38 +1,35 @@
 package com.veselkova.androidlivewallpaper;
 
-import android.graphics.Color;
-
-/**
- * Created by purch on 5/29/2017.
- */
-
-public class MyPoint {
-    double speed;
-    float radius;
-    double life;
-    double rlife;
-    double rr, gg, bb;
-    int r;
-    int g;
+public class MyPoint
+{
     int b;
-    float x, y;
+    double bb;
+    int g;
+    double gg;
+    double life;
+    int r;
+    float radius;
+    double rlife;
+    double rr;
+    double speed;
     String text;
+    float x;
+    float y;
 
-    public MyPoint(String text, float x, float y)
+    public MyPoint(String paramString, float paramFloat1, float paramFloat2)
     {
-        this.text=text;
-        this.y=y;
-        this.x=x;
-        speed = -2.5+Math.random()*5;
-        radius = (float)(10+Math.random()*20);
-        life = 20+Math.random()*10;
-        rlife = life;
-        rr= Math.floor(Math.random()*255) ;
-        gg = Math.floor(Math.random()*255);
-        bb =Math.floor(Math.random()*255);
-        r = (int)rr;
-        g=(int) gg;
-        b = (int)bb;
-
+        this.text = paramString;
+        this.y = paramFloat2;
+        this.x = paramFloat1;
+        this.speed = (-2.5D + Math.random() * 5.0D);
+        this.radius = ((float)(Math.random() * 20.0D + 10.0D));
+        this.life = (Math.random() * 10.0D + 20.0D);
+        this.rlife = this.life;
+        this.rr = Math.floor(Math.random() * 255.0D);
+        this.gg = Math.floor(Math.random() * 255.0D);
+        this.bb = Math.floor(Math.random() * 255.0D);
+        this.r = ((int)this.rr);
+        this.g = ((int)this.gg);
+        this.b = ((int)this.bb);
     }
 }
